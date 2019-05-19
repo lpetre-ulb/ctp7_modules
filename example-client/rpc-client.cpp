fa7a9aeed49384f4455e614023b88acb72808e7c
Laurent Pétré <lpetre@ulb.ac.be>
2019-05-20 00:20:24 +0200

    Update the example client to read the Virtex-7 release

--- example-client/rpc-client.cpp
+++ example-client/rpc-client.cpp
@@ -5,17 +5,23 @@
 
 int main(int argc, char **argv)
 {
-    RPC::Connection conn;
-    conn.connect("localhost");
+    try {
+        RPC::Connection conn;
+        conn.connect("localhost");
 
-    conn.load_module("memory", "memory v1.0.1");
+        conn.load_module("memory", "memory v1.0.1");
 
-    auto mem = conn.call<Memory::Read>(0, 10);
-    std::cout << std::hex;
-    for (std::uint32_t word : mem) {
-        std::cout << " " << word;
+        auto mem = conn.call<Memory::Read>(0x6640000c, 1);
+        std::cout << std::hex;
+        for (std::uint32_t word : mem) {
+            std::cout << "CTP7 Virtex-7 firmware release : " << word;
+        }
+        std::cout << std::endl;
+    } catch (const wisc::RPCSvc::RPCException &e) {
+        std::cout << e.message << std::endl;
+    } catch (const std::exception &e) {
+        std::cout << e.what() << std::endl;
     }
-    std::cout << std::endl;
 
     return 0;
 }
